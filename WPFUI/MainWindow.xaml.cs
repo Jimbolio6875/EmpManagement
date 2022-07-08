aec597164d45670166d0b181848d150a677a9205
Kyan Carr <kyancarr.51202@outlook.com>
2022-07-08 17:30:32 -0500

    changed design from ModernUI to HandyControl and got Datagrid to display data

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HandyControl.Data;
+using HandyControl.Themes;
+using HandyControl.Tools;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,15 +16,59 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using FirstFloor.ModernUI.Windows.Controls;
 
 namespace WPFUI
 {
-
-    public partial class MainWindow : ModernWindow
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateSkin(SkinType.Dark);
+            GetData();
+        }
 
+        public void UpdateSkin(SkinType skin)
+        {
+            SharedResourceDictionary.SharedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
+            });
+            Application.Current.MainWindow?.OnApplyTemplate();
+        }
+
+        private void GetData()
+
+        {
+
+            string connString = ConfigurationManager.ConnectionStrings["connEmployee"].ConnectionString;
+
+            string cmdString = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(connString))
+
+            {
+
+                cmdString = "SELECT ID, FirstName, LastName FROM Employee";
+
+                SqlCommand sqlCmd = new SqlCommand(cmdString, con);
+
+                SqlDataAdapter sda = new SqlDataAdapter(sqlCmd);
+
+                DataTable dt = new DataTable("Employee");
+
+                sda.Fill(dt);
+
+                EmployeeDataGrid.ItemsSource = dt.DefaultView;
+
+            }
+
+        }
     }
 }
